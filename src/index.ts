da641179f51eacc65bf0985688c438c956c496f8
Jesus Hidalgo <jhidgon0808@iescarrilo.es>
2025-09-29 09:25:49 +0200

    Arrays

--- src/index.ts
+++ src/index.ts
@@ -59,3 +59,21 @@ if (typeof variableDesconocida==="string"){
 console.log("Tu nombre es:"+nombre);
 console.log(`Tu edad es: ${edad}`);
 //console.log(variableDesconocida.toUpperCase());
+
+/**
+ * ARRAYS
+ */
+
+let numeros:number[]=[1,2,3,4,5];
+let numeros2:number[]=[5,6,7,8,9];
+
+let mixto: (number|string)[]=[1,"Hola",2,"Adios"];
+
+let array2d: number[][]=[[1,2,3],[4,5,6],[7,8,9]];
+
+console.log(`La posicion 0 del array numeros es: ${numeros[0]}`);
+
+console.log(`La posicion 0 del array 2d es: ${array2d[0]!=undefined ? array2d[0][0]:0}`);
+
+let numeros3 = [...numeros,...numeros2];
+console.log(`La concatenacion de los arrays numeros  numeros2 es: ${numeros3}`);
